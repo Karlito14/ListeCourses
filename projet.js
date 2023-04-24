fa4036b37692f067decd0bc5675177426c2cd5be
Karlito14 <leiroz26@hotmail.com>
2023-04-24 15:00:32 +0200

    stockage de la liste dans le local storage

--- projet.js
+++ projet.js
@@ -3,62 +3,75 @@ const formulaire = document.querySelector('form');
 // Recupérer la liste ul
 const listeUl = document.querySelector('#liste');
 // Récupérer la référence du template
-const templateItem = document.querySelector('#template-item')
-
+const templateItem = document.querySelector('#template-item');
+const maListe = [];
 
 const addNewItem = () => {
+    const liste = localStorage.getItem('liste');
+    console.log(liste)
     // Cloner l'élément li du template
     const elementLi = templateItem.content.cloneNode(true);
 
+    const objetItem = {
+        nom : null,
+        quantité : 1,
+        unite : 'u.',
+    }
+
     // Recupère la valeur du nouvel item
-    let valueItem = inputNewItem.value; 
+    objetItem.nom = inputNewItem.value; 
 
     // Supprimer les espaces avant et après de l'item
-    valueItem = valueItem.trim();
+    objetItem.nom = objetItem.nom.trim();
 
     // Supprimer les espaces superficiels au milieu de la string
-    while(valueItem.indexOf("  ") !== -1){
-        valueItem = valueItem.replace("  ", " ");
+    while(objetItem.nom.indexOf("  ") !== -1){
+        objetItem.nom = objetItem.nom.replace("  ", " ");
     }
 
     // Insertion intelligente
     // Division de ma string en tableau
-    const tableauSplit = valueItem.split(' ');
+    const tableauSplit = objetItem.nom.split(' ');
     // Récupération du premier mot et conversion en nombre si c'est possible
     const premierMot = +tableauSplit[0];
-    // Test si c'est un nombre si c'est le cas 
+    // Test si c'est un nombre 
     if(Number.isInteger(premierMot)){
         const elementQuantite = elementLi.querySelector('.quantite');
-        elementQuantite.textContent = premierMot;
+        objetItem.quantité = premierMot;
+        elementQuantite.textContent = objetItem.quantité ;
         // Je retire le 1er élément du tableau
         tableauSplit.shift();
-
+        
         // test si le 2ème mot est une unité
         const selectOptions = elementLi.querySelector('.unite');
         for (let i = 1 ; i < selectOptions.length; i++) {
             const valueOptions = selectOptions[i].value;
             // Je vérifie si le deuxième est une unité
             if(tableauSplit[0].includes(valueOptions)) {
-                const unite = tableauSplit[0];
+                objetItem.unite = tableauSplit[0];
                 tableauSplit.shift();
-                selectOptions.value = unite;
+                selectOptions.value = objetItem.unite;
+                break;
             }
         }
-
     }
 
     // Je concatène tous les élements du tableau 
-    valueItem = tableauSplit.join(' ');
+    objetItem.nom = tableauSplit.join(' ');
 
     // Mettre la 1ere lettre en majuscule
-    const majuscule = valueItem[0].toUpperCase();
-    valueItem = majuscule + valueItem.slice(1);
+    const majuscule = objetItem.nom[0].toUpperCase();
+    objetItem.nom = majuscule + objetItem.nom.slice(1);
+
+    // stockage de ma liste dans mon local storage
+    maListe.push(objetItem);
+    localStorage.setItem('liste', JSON.stringify(maListe));
     
     // Récupérer le noeud paragraphe dans le li
     const elementNom = elementLi.querySelector('.nom-item');
     
     // Insérer la valeur du input dans le noeud paragraphe
-    elementNom.textContent = valueItem;
+    elementNom.textContent = objetItem.nom;
     
     // Rattacher l'élément li au noeud ul
     listeUl.appendChild(elementLi);
